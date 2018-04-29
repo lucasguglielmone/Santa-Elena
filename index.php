17e1620878ffe6384abd53d67a6a548e63cada2b
igustin10 <37119096+igustin10@users.noreply.github.com>
2018-04-29 18:36:32 -0300

    index y nav corregido

--- index.php
+++ index.php
@@ -13,162 +13,59 @@
 	<div id="fh5co-work">
 		<div class="container">
 			<div class="row top-line animate-box">
-				<div class="col-md-7 col-md-push-5 text-left intro">
-					<h2>Conocé los productos de la mejor confiteria de capital! <span class="fh5co-highlight"> <i class="icon-heart2"></i> <a href="http://freehtml5.co">ANABELLA</a> </span></h2>
+				<div class="col-md-12 text-left intro"> <!-- col-md-push-5 -->
+					<h2>Desde 1915 ofreciendo el mejor servicio<span class="fh5co-highlight"> <i class="icon-heart2"></i> <a >SANTA ELENA</a> </span></h2>
 					<!-- <h2>Shift is a Collection of a Beautiful &amp; Premium Themes.</h2> -->
 				</div>
 				
 			</div>
 			<div class="row">
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-1.jpg);">
-							<div class="inner">
-								<div class="desc">
-								<h3>Producto 1</h3>
-								<span class="cat">PAN</span>
-							</div>
-							</div>
+					<a class="work">
+						<div class="work-grid" style="background-image:url(images/index/1.jpg);">
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-2.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Producto 2</h3>
-									<span class="cat">TORTA</span>
-								</div>
-							</div>
+					<a class="work">
+						<div class="work-grid" style="background-image:url(images/index/2.jpg);">
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-3.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Producto 3</h3>
-									<span class="cat">MORNE</span>
-								</div>
-							</div>
+					<a class="work">
+						<div class="work-grid" style="background-image:url(images/index/3.jpg);">
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-4.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Electric Kick Scooter</h3>
-									<span class="cat">Web Design</span>
-								</div>
-							</div>
+					<a class="work">
+						<div class="work-grid" style="background-image:url(images/index/4.jpg);">
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-5.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Shark Diver</h3>
-									<span class="cat">Branding</span>
-								</div>
-							</div>
+					<a class="work">
+						<div class="work-grid" style="background-image:url(images/index/5.jpg);">
 						</div>
 					</a>
 				</div>
 				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-6.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Objekte Unserer Tage Berlin</h3>
-									<span class="cat">Logo</span>
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-7.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Aloha Shirt</h3>
-									<span class="cat">Web Design</span>
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-8.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Olafur Elliasson Waterfall Versailles</h3>
-									<span class="cat">Branding</span>
-								</div>
-							</div>
-						</div>
-					</a>
-				</div>
-				<div class="col-md-4 text-center animate-box">
-					<a class="work" href="portfolio_detail.html">
-						<div class="work-grid" style="background-image:url(images/project-9.jpg);">
-							<div class="inner">
-								<div class="desc">
-									<h3>Hand Care</h3>
-									<span class="cat">Logo</span>
-								</div>
-							</div>
+					<a class="work">
+						<div class="work-grid" style="background-image:url(images/index/6.jpg);">
 						</div>
 					</a>
 				</div>
 			</div>
 		</div>
 	</div>
-	
-	<div id="fh5co-author" class="fh5co-bg-section">
-		<div class="container">
-			<div class="row animate-box">
-				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-					<h2>A little about me</h2>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2">
-					<div class="author">
-						<div class="author-inner animate-box" style="background-image: url(images/person3.jpg);">
-						</div>
-						<div class="desc animate-box">
-							<span>Web Designer &amp; Developer</span>
-							<h3>Mike Airways</h3>
-							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-							<p><a href="about.html" class="btn btn-primary btn-outline">Learn More</a></p>
-							<ul class="fh5co-social-icons">
-								<li><a href="#"><i class="icon-facebook"></i></a></li>
-								<li><a href="#"><i class="icon-twitter"></i></a></li>
-								<li><a href="#"><i class="icon-dribbble"></i></a></li>
-								<li><a href="#"><i class="icon-github"></i></a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
 	<div id="fh5co-started">
 		<div class="container">
 			<div class="row animate-box">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-					<h2>Get Started</h2>
-					<p>We create beautiful themes for your site behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-					<p><a href="#" class="btn btn-primary">Let's work together</a></p>
+					<h2>HACE TU PEDIDO DE CATERING</h2>
+					<p>Ofrecemos el mejor serivio de catering, recordá que tu pedido debe realizarse con 2 dias de anticipación.</p>
+					<p><a href="#" class="btn btn-primary">ORDENAR AHORA</a></p>
 				</div>
 			</div>
 		</div>
@@ -179,17 +76,9 @@
 			<div class="row copyright">
 				<div class="col-md-12 text-center">
 					<p>
-						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a> &amp; <a href="http://blog.gessato.com/" target="_blank">Gessato</a></small>
-					</p>
-					
-					<ul class="fh5co-social-icons">
-						<li><a href="#"><i class="icon-twitter"></i></a></li>
-						<li><a href="#"><i class="icon-facebook"></i></a></li>
-						<li><a href="#"><i class="icon-linkedin"></i></a></li>
-						<li><a href="#"><i class="icon-dribbble"></i></a></li>
-					</ul>
-					
+						<small class="block">&copy; 2018 Santa Elena. Todos los derechos reservados.</small> 
+						<small class="block">Designed by <a href="http://cegeinc.com/" target="_blank">CeGeInc.com</a></small>
+					</p>					
 				</div>
 			</div>
 
--- nav.php
+++ nav.php
@@ -10,16 +10,14 @@
     </div>
     <div class="fh5co-top-menu menu-1 text-center">
         <ul>
-            <li><a href="work.html">Productos</a></li>
-            <li><a href="about.html">Quienes Somos</a></li>
+            <li><a href="historia.html">Historia</a></li>
+            <li><a href="pedido.html">Pedido Catering</a></li>
             <li><a href="sucursales.php">Sucursales</a></li>
             <li class="has-dropdown">
                 <a href="#">Productos</a>
                 <ul class="dropdown">
-                    <li><a href="#">Panaderia</a></li>
-                    <li><a href="#">Confiteria</a></li>
-                    <li><a href="#">Tortas</a></li>
-                    <li><a href="#">Morne</a></li>
+                    <li><a href="#">Catering Salado</a></li>
+                    <li><a href="#">Catering Dulce</a></li>
                 </ul>
             </li>
             <li><a href="contact.html">Contacto</a></li>
@@ -27,9 +25,9 @@
     </div>
     <div class="fh5co-top-social menu-1 text-right">
         <ul class="fh5co-social">
-            <li><a href="#"><i class="icon-twitter"></i></a></li>
+            <!-- <li><a href="#"><i class="icon-twitter"></i></a></li>
             <li><a href="#"><i class="icon-dribbble"></i></a></li>
-            <li><a href="#"><i class="icon-github"></i></a></li>
+            <li><a href="#"><i class="icon-github"></i></a></li> -->
         </ul>
     </div>
 </div>
