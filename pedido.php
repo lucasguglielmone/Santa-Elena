f3df8ab6451979ed64c09df0437b9903063513e2
Lucas Guglielmone <LGRPARG@GMAIL.COM>
2018-07-01 17:00:04 -0300

    repeat

--- pedido.php
+++ pedido.php
@@ -208,3 +208,14 @@ $(document).ready(function(){
     });
 });
 </script>
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
