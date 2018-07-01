667bc834e0e1d3ed3a296e64e4f017132184a316
Lucas Guglielmone <LGRPARG@GMAIL.COM>
2018-07-01 15:17:38 -0300

    Fix favicon

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 <head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<link rel="icon" href="/santaelena/logo.png">
+		<link rel="icon" href="/santa-elena/logo.png">
 		<title>Santa Elena</title>
 
 
